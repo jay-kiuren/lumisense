df559479a3ef6a4f90336a9362b5dbf6c1073d6f
gnobob <lillodominador@gmail.com>
2026-09-08 14:52:43 +0800

    updating stat report

--- lib/features/statistics/presentation/statistics_report_page.dart
+++ lib/features/statistics/presentation/statistics_report_page.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:math' show log;
 
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../core/export/csv_export.dart';
