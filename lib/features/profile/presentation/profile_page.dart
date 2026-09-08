7f23d65d60ef92efafa96d97a5dc88fc92a80362
gnobob <lillodominador@gmail.com>
2026-09-08 14:46:00 +0800

    updating profile page

--- lib/features/profile/presentation/profile_page.dart
+++ lib/features/profile/presentation/profile_page.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import '../../../core/theme/app_colors.dart';
