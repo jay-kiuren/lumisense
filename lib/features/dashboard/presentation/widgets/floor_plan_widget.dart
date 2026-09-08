fb80dfb6725bf62e3608546ce38b43f0c3409e77
gnobob <lillodominador@gmail.com>
2026-09-08 14:41:36 +0800

    updating library on floor plan

--- lib/features/dashboard/presentation/widgets/floor_plan_widget.dart
+++ lib/features/dashboard/presentation/widgets/floor_plan_widget.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 import '../../../../core/theme/app_colors.dart';
 
