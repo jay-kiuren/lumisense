202cc4981b21070187b63fe4f4e74a8dcbe2e2bf
gnobob <lillodominador@gmail.com>
2026-09-08 14:36:30 +0800

    updating library on department card

--- lib/features/dashboard/presentation/widgets/department_card.dart
+++ lib/features/dashboard/presentation/widgets/department_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import '../../../../core/domain/entities/zone_snapshot.dart';
 import '../../../../core/domain/value_objects/noise_level.dart';
 import '../../../../core/services/settings_service.dart';
