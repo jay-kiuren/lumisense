d81cbfc9350ffdeace743bfddc1a182da87d79f7
gnobob <lillodominador@gmail.com>
2026-09-08 14:46:31 +0800

    updating sidebar

--- lib/features/shared/presentation/widgets/sidebar.dart
+++ lib/features/shared/presentation/widgets/sidebar.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../../../core/theme/app_colors.dart';
 import '../../../auth/presentation/login_page.dart';
